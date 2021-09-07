06fc9cc88ef7f85e861da97b927c875432022d5d
Vishal <vishal.aditya6@gmail.com>
2021-09-07 14:55:49 +0530

    Create Spring Boot Project

---
+++ src/test/java/guru/framework/spring5jokesapp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
